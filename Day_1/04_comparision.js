189fc007f947dfaf1b7bcce0094ad7f448eb19e9
Ashwin Gadhvi <146506495+AshwinGadhvi@users.noreply.github.com>
2024-06-10 08:03:08 +0000

    Practice with some comparision based on some condition and practice more with identical equalsTo

---
+++ Day_1/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1); // javascript automatically convert that string into number
+// console.log("02">1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+//console.log(undefined == 0);
+
+// === it is also called identical equal to it checks values with their datatype
+
+// console.log("2"===2);
