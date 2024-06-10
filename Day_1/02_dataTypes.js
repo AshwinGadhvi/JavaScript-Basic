48fceedeba57b24c2af8b0449515c914381213f0
Ashwin Gadhvi <146506495+AshwinGadhvi@users.noreply.github.com>
2024-06-10 06:44:06 +0000

    Declaration of variable using dataTypes

---
+++ Day_1/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js Code as newer version
+
+// alert(3+3) // we are using nodeJs, not browser
+
+//console.log(3+3); // ecmascript tc39 and mdn watch this to 
+
+let name = "Ashwin"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// Number => 2 to power 53
+// bigInt
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => used for uniqueness
+
+// Object
+
+console.log(typeof null); // type of this variable is object
+console.log(typeof undefined); // type of this variable is undefined
