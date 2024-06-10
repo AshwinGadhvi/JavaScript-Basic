6c84f1ae9b718f557b1bec1f36841ef32255eabb
Ashwin Gadhvi <146506495+AshwinGadhvi@users.noreply.github.com>
2024-06-10 06:17:31 +0000

    Basic Variables Declaration

---
+++ Day_1/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accoutEmail = "ashwingadhvi.ce@gmail.com"
+var accoutnPass = "4323"
+acccoutCity = "Adipur"
+let accountState;
+
+// accountId = 2 // this is not allowed in the javascript
+
+accoutEmail = "ag@gmail.com"
+accoutnPass= "4455"
+acccoutCity = "Anjar"
+
+console.log(accountId);
+
+console.table([accountId,accoutEmail,accoutnPass,acccoutCity,accountState]); // to log multiple variables at once we use table method with [].
+
+/*
+prefferd not used to var
+Because of issue in block scope and functional scope
+*/
