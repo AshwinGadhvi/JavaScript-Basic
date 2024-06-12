82a6bb5279dbba4a980ef9179503a50f585b447d
Ashwin Gadhvi <146506495+AshwinGadhvi@users.noreply.github.com>
2024-06-12 07:17:14 +0000

    UnderStading Object DeStructuring And Also Learn About JSON. And Learn Breif About API.

---
+++ Day_3/02_DeStructure.js
@@ -0,0 +1,27 @@
+const course = {
+    courseName: "JavaScript",
+    courseFee: "1000",
+    courseInstructor: "Ashwin"
+}
+
+const {courseInstructor: c_i} = course // doing de strucutre of courseINstructore
+// console.log(courseInstructor);
+console.log(c_i)
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++ Application Program Interface++++++++++++++++++++++++++++++++++++++
+
+// ==> JSON => JavaScript Object Notation
+// ==> Syntax
+
+// {
+//     "name":"Ashwin",
+//     "courseName":"JavaScript",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
