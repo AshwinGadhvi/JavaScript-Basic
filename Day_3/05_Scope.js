6b551538ae5b6100164f9badde88e4ba2f706f85
Ashwin Gadhvi <146506495+AshwinGadhvi@users.noreply.github.com>
2024-06-12 08:36:07 +0000

    Learning About Basic Scope

---
+++ Day_3/05_Scope.js
@@ -0,0 +1,19 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+if(true)
+{
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a); // it was not accessible
+// console.log(b); // it was not accessible
+// console.log(c); // it was accessible which is inappropriate
