7f26f18c3ae5cc8bc9d1c1a96018682062c51be9
Ashwin Gadhvi <146506495+AshwinGadhvi@users.noreply.github.com>
2024-06-19 10:06:36 +0000

    Updated File Content

--- Mini-Projects/01_backgroundChanger/app.js
+++ Mini-Projects/01_backgroundChanger/app.js
@@ -3,7 +3,7 @@ const body = document.querySelector('body');
 console.log(button);
 
 button.forEach((item)=>{
-    item.addEventListener("click",function op(e){
+    item.addEventListener("click",function (e){
         if (e.target.id === 'blue')
         {
             body.style.backgroundColor = e.target.id;
@@ -22,3 +22,10 @@ button.forEach((item)=>{
         }
     });
 });
+
+
+const home = document.querySelector('#Home');
+
+home.addEventListener('click',()=>{
+    window.location.href = "../index.html";
+})
